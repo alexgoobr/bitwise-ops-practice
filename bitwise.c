fd65afb0625916f0fc9b28af4506fb656368c291
herrgoobz <ine15ago@student.lu.se>
2020-01-01 11:09:26 -0400

    added rules

--- bitwise.c
+++ bitwise.c
@@ -1,4 +1,49 @@
-/* 
+/*
+  RULES:
+
+  Your code must conform to the following style:
+
+  int Funct(arg1, arg2, ...) {
+    int var1 = Expr1;
+    ...
+    int varM = ExprM;
+
+    varJ = ExprJ;
+    ...
+    varN = ExprN;
+    return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting an integer by more
+     than the word size.
+ */
+
+ /*
  * bang - Compute !x without using !
  *   Examples: bang(3) = 0, bang(0) = 1
  *   Legal ops: ~ & ^ | + << >>
